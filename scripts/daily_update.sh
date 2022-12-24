355c180c1fff15c0b040d73dc38a491a60b79f99
charles chen <szcharlie@gmail.com>
2022-12-24 13:15:44 +0800

    更新 数据

--- scripts/daily_update.sh
+++ scripts/daily_update.sh
@@ -4,7 +4,7 @@ cd /home/charles/icyChessZero/scripts
 count=`ls ../data/distributed | wc -l`
 echo ${count}
 if [ $count -le 10000 ];then
-    echo 'exiting'
+    echo 'less than 10000 exiting'
     exit
 fi 
 
@@ -12,12 +12,21 @@ sh kill_all.sh
 #sleep 10s
 #sh multithread_start.sh -p /usr/local/bin/python3  -g 1 -t 20 -n no
 sleep 10s
+
+echo 'executing model_update...'
 python model_update.py -g 0 > update_log.txt
 sleep 10s
+
 sh kill_all.sh
 sleep 10s
+
+echo 'executing model_upweight...'
 python upweight.py > uplog.tt
 sleep 10s
+
+echo 'executing validate...'
+date
+
 sh validate.sh -p python -g 0 -t 14 > validate_log.txt #&
 #sh validate.sh -p /usr/local/bin/python3 -g 1 -t 20 > validate_log.txt 
 sleep 10s
